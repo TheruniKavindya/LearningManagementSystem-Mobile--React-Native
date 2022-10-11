bb44ee36c05f8064fb4185d5493b945c7e46ca83
TheruniKavindya <theruniks1998@gmail.com>
2022-10-11 22:48:31 +0530

    first commit

---
+++ screens/index.js
@@ -0,0 +1,7 @@
+// Dashboard
+import MainLayout from "./Dashboard/MainLayout";
+import Home from "./Dashboard/Home";
+import Profile from "./Dashboard/Profile";
+import Search from "./Dashboard/Courses";
+
+export { MainLayout, Home, Profile, Search };
